26cc94b8ea76a8dbb103b3136e28796535d8cbe0
Eduardo M <107496045+EMSMoreno@users.noreply.github.com>
2024-06-27 15:25:16 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using Project_Async_WPF;
+using System.Net;
+using System.Windows;
+
+namespace DemoAsync_WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void executaSync_Click(object sender, RoutedEventArgs e)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            RunDownloadSync();
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            resultsWindow.Text += $"Tempo de execução Total: {elapsedMs}{Environment.NewLine}";
+        }
+
+        private List<string> PrepData()
+        {
+            List<string> output = new List<string>
+            {
+                "https://www.yahoo.com",
+                "https://www.google.com",
+                "https://www.microsoft.com",
+                "https://www.cnn.com",
+                "https://www.codeproject.com"
+            };
+
+            resultsWindow.Text = "";
+
+            return output;
+        }
+
+        private async Task RunDownloadSync()
+        {
+            List<string> websites = PrepData();
+
+            foreach (string site in websites)
+            {
+                WebsiteDataModel results = DownloadWebsite(site);
+                ReportWebsiteInfo(results);
+            }
+        }
+
+        private WebsiteDataModel DownloadWebsite(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+
+            output.WebsiteURL = websiteURL;
+            output.WebsiteContent = client.DownloadString(websiteURL);
+
+            return output;
+        }
+
+        private void ReportWebsiteInfo(WebsiteDataModel data)
+        {
+            resultsWindow.Text += $"O site {data.WebsiteURL} fez download de: {data.WebsiteContent.Length} caracteres. {Environment.NewLine}";
+        }
+
+        private async void executaAsync_Click(object sender, RoutedEventArgs e)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            await RunDownloadAsync();
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            resultsWindow.Text += $"Tempo de execução Total: {elapsedMs}";
+        }
+
+        private async Task RunDownloadAsync()
+        {
+            List<string> websites = PrepData();
+
+            foreach (string site in websites)
+            {
+                WebsiteDataModel results = await Task.Run(() => DownloadWebsite(site));
+                ReportWebsiteInfo(results);
+            }
+        }
+
+        private async Task RunDownloadParallelAsync()
+        {
+            List<string> websites = PrepData();
+            List<Task<WebsiteDataModel>> tasks = new List<Task<WebsiteDataModel>>();
+
+            foreach (string site in websites)
+            {
+                tasks.Add(DownloadWebsiteAsync(site));
+            }
+
+            var results = await Task.WhenAll(tasks);
+
+            foreach (var item in results)
+            {
+                ReportWebsiteInfo(item);
+            }
+        }
+
+        private async Task<WebsiteDataModel> DownloadWebsiteAsync(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+
+            output.WebsiteURL = websiteURL;
+            output.WebsiteContent = await client.DownloadStringTaskAsync(websiteURL);
+
+            return output;
+        }
+    }
+}
---
+++ WebsiteDataModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Async_WPF
+{
+    public class WebsiteDataModel
+    {
+        public string WebsiteURL { get; set; }
+        public string WebsiteContent { get; set; }
+    }
+}
